70e21c73aa73a4c4ff42d13c3280555fb7741f12
NKL110 <leena03@ewha.ac.kr>
2026-09-23 03:04:54 +0000

    message

--- main.cpp
+++ main.cpp
@@ -3,12 +3,20 @@
 namespace  LeeNaKyung2593202 {
     //1의 본인이름학번의 네임스페이스 안에 비멤버함수 compare클래스1 정의: 
     // 매개변수는 const 클래스1 참조형 2개, 매개변수 멤버들이 모두 같은지를 비교
-    bool
+    bool compareStudent(const student&s1, const student&s2)
 
+    
+}
+
+int main(){
+    using namespace LeeNaKyung2593202;
+    studentstatus s1;
+    s1.print();
     //클래스2 객체1 선언, print함수 호출
 
     //클래스2 객체2 초기값을 넣어서 선언, print함수 호출
 
     //비멤버함수 compare클래스1을 호출하여 그 리턴값이 true면 same,
     // false면 not same을 표준스트림으로 출력
+    return 0;
 }
--- s_statuscode.h
+++ s_statuscode.h
@@ -9,22 +9,13 @@ class class2{
     student s;
     bool status;
     int classcode;
+
     
-    void testStatus(){
-        if (status =! true || status =! false){
-            std::cout << "Invalid status!" << std::endl;
-            std::exit(1);}
-    }
-    void testClasscode(){
-        if (classcode < 10000 || classcode > 99999){
-            std::cout << "Invalid classcode!" << std::endl;
-            std::exit(1);}
-    }
 
     public:
 //public 멤버함수 인라인으로 정의
     studentstatus(student s0= student{1234567,0,'F'}, bool st=false, int c=12345)
-        :s{s0},status{st},
+        :s{s0}, status{st}, classcode{c}
     //-생성자: 모든 멤버변수 초기화, 기본값 설정, test함수들 호출
    // studentstatus(student s0= student{1234567,0,'F'}, bool st=false, int c=12345)
         //:s{d,s,g},status{st},
