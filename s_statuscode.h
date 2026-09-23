cb9f5772e1526d019b49e90559e3c3d759d35a1d
NKL110 <leena03@ewha.ac.kr>
2026-09-23 03:09:38 +0000

    message

--- s_statuscode.h
+++ s_statuscode.h
@@ -3,7 +3,7 @@
 
 namespace LeeNaKyung2593202 {
 
-class class2{
+class studentstatus{
 
     private:
     student s;
@@ -16,6 +16,8 @@ class class2{
 //public 멤버함수 인라인으로 정의
     studentstatus(student s0= student{1234567,0,'F'}, bool st=false, int c=12345)
         :s{s0}, status{st}, classcode{c}
+
+        {}
     //-생성자: 모든 멤버변수 초기화, 기본값 설정, test함수들 호출
    // studentstatus(student s0= student{1234567,0,'F'}, bool st=false, int c=12345)
         //:s{d,s,g},status{st},
@@ -25,9 +27,10 @@ void print() const{
     s.print(); //student::print()
     if (status) std::cout<<"on school\n";
     else std:: cout<<"NOT on school\n";
+    std::cout << "class code: " << classcode << '\n';
 }
 // -클래스1형 객체의 접근함수를 참조형식으로 구현
-student getStudent() const {return s;}
+student& getStudent() {return s;}
 void setStudent(const student&s0){s=s0;}
  };
 
