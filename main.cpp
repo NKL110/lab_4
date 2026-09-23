7b28cc9f69dc66c86dc3cdf864696219f138996f
NKL110 <leena03@ewha.ac.kr>
2026-09-23 02:59:11 +0000

    message

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "s_statuscode.h"
+
+namespace  LeeNaKyung2593202 {
+    //1의 본인이름학번의 네임스페이스 안에 비멤버함수 compare클래스1 정의: 
+    // 매개변수는 const 클래스1 참조형 2개, 매개변수 멤버들이 모두 같은지를 비교
+    bool
+
+    //클래스2 객체1 선언, print함수 호출
+
+    //클래스2 객체2 초기값을 넣어서 선언, print함수 호출
+
+    //비멤버함수 compare클래스1을 호출하여 그 리턴값이 true면 same,
+    // false면 not same을 표준스트림으로 출력
+}
